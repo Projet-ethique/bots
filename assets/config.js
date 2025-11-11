b7c9a2384cc4bf2d8f0f763297e1ab68b5962aec
Nix177 <nicolas.tuor@bluewin.ch>
2025-11-11 13:58:51 +0100

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,4 +1,2 @@
 // assets/config.js
-// URL de ton Worker Cloudflare (API)
 export const API_BASE = "https://dark-rice-25ee.nicolas-tuor.workers.dev/api";
-// Endpoints utilisés : `${API_BASE}/chat` et `${API_BASE}/save`
